2c9089c8be741a93336b46e8d18d6b9bfbac9e2a
Alice Fung <alice.o.fung@gmail.com>
2015-08-10 16:04:36 -0400

    change some config settings

--- photo_emails_delays/config/environments/development.rb
+++ photo_emails_delays/config/environments/development.rb
@@ -42,11 +42,11 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin"
 
   config.paperclip_defaults = {
+    :storage => :s3,
     :s3_credentials => {
-      :secret_access_key => Rails.application.secrets.aws_secret_access_key,
-      :s3_bucket_name => Rails.application.secrets.
-        s3_bucket_name,
-      :aws_access_key_id => Rails.application.secrets.aws_access_key_id
+      :bucket => Rails.application.secrets.s3_bucket_name,
+      :access_key_id => Rails.application.secrets.aws_access_key_id,
+      :secret_access_key => Rails.application.secrets.aws_secret_access_key
 
     }
   }
